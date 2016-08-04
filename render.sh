d5fc13415da185e4153e5b14b5f6fafdefcce690
Benjamin Soltoff <ben@bensoltoff.com>
2016-08-04 15:04:20 -0500

    Move "last updated" to YAML rather than a hacked footer

--- render.sh
+++ render.sh
@@ -6,9 +6,5 @@ for notebook in $( ls *.ipynb ); do
 done
 
 # render_site using rmarkdown
-## update footer
-Rscript -e 'rmarkdown::render("_footer.Rmd", "html_fragment")'
-
-## now render entire website
 # Rscript -e 'rmarkdown::render_site()'
 Rscript render.R FALSE
