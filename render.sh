48ff9ee7c8e2078b0ffcf86998cfe73a793a8e04
Benjamin Soltoff <ben@bensoltoff.com>
2016-09-09 09:37:57 -0500

    Add component to `render.sh` to copy slides from teaching materials directory to website folder

--- render.sh
+++ render.sh
@@ -5,6 +5,10 @@ for notebook in $( ls *.ipynb ); do
   /Users/$USER/anaconda/bin/jupyter nbconvert --to markdown $notebook
 done
 
+# copy slides to extras folder
+for slides in $( find ../teach -name "*slides.html" ); do
+  rsync --update $slides extras
+done
+
 # render_site using rmarkdown
 Rscript -e 'rmarkdown::render_site()'
-# Rscript render.R FALSE
