cfc94e464e8a6f14023c3c86eca7a406038f287f
Benjamin Soltoff <ben@bensoltoff.com>
2016-12-29 09:11:53 -0600

    Update render.sh to account for slides being stored in extras folder

--- render.sh
+++ render.sh
@@ -1,25 +1,10 @@
 #!/bin/bash          
 
-# move slides to protected folder
-for slides in $( find ./ -maxdepth 1 -name "*slides.Rmd" ); do
-  mv $slides extras/$slides
-done
-
-# render_site using rmarkdown
+# render site using rmarkdown
 Rscript -e 'rmarkdown::render_site()'
 
-# move slides back to main directory and render
-for slides in $( find extras/ -name "*slides.Rmd" ); do
-  # move slides back
-  filename=$(basename "$slides")
-  mv extras/$filename ./$filename
-  
-  # rename _site.yml temporarily to properly render slides
-  mv _site.yml site.yml
-  
+# render slides in extras
+for slides in $( find extras -name "*slides.Rmd" ); do
   # render slides
-  Rscript -e "rmarkdown::render('$filename')"
-  
-  # rename _site.yml back
-  mv site.yml _site.yml
+  Rscript -e "rmarkdown::render('$slides')"
 done
