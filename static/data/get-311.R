789eb13a1fc5f55f5d50f0a2651f1e9924596fe0
Benjamin Soltoff <ben@bensoltoff.com>
2022-09-01 11:02:10 -0400

    Get NYC data for spatial viz exercises and demos

--- static/data/get-311.R
+++ static/data/get-311.R
@@ -1,24 +1,33 @@
 ## get-311.R
 ## 
-## Use the Chicago Data Portal API to get 311 reports for dead animals and potholes
+## Use the NYC Data Portal API to get 311 reports and crime data
 
 library(tidyverse)
 library(RSocrata)
 library(here)
 
-# use API to get 311 complaints for dead animals and potholes
-short_codes <- c("SGQ", "PHF")
-short_codes_urls <- str_c(
-  "https://data.cityofchicago.org/resource/v6vf-nfxy.json?sr_short_code=",
-  short_codes
-)
-
-# generate all queries
-chi_311_full <- map_df(short_codes_urls, read.socrata) %>%
+# use API to get 311 complaints for
+# - Food Poisoning
+# - Sidewalk Condition
+nyc_311 <- read.socrata(
+  url = "https://data.cityofnewyork.us/resource/erm2-nwe9.json?$where=complaint_type in('Food Poisoning', 'Sidewalk Condition')"
+) %>%
   as_tibble()
 
 # clean up/shrink the dataset for exercises
-chi_311_full %>%
-  select(starts_with("sr"), -sr_type, created_date, community_area, ward, latitude, longitude) %>%
-  mutate(across(.cols = c(community_area, ward, latitude, longitude), .fns = as.numeric)) %>%
-  write_csv(here("data", "chicago-311.csv"))
+nyc_311 %>%
+  select(unique_key, created_date, complaint_type, borough, latitude, longitude) %>%
+  mutate(across(.cols = c(latitude, longitude), .fns = as.numeric)) %>%
+  write_csv(here("static", "data", "nyc-311.csv"))
+
+# use API to get year to date NYPD complaint data
+nyc_crimes <- read.socrata(
+  url = "https://data.cityofnewyork.us/resource/5uac-w243.json"
+) %>%
+  as_tibble()
+
+# clean up/shrink the dataset for demos
+nyc_crimes %>%
+  select(cmplnt_num, boro_nm, cmplnt_fr_dt, law_cat_cd, ofns_desc, latitude, longitude) %>%
+  mutate(across(.cols = c(latitude, longitude), .fns = as.numeric)) %>%
+  write_csv(here("static", "data", "nyc-crimes.csv"))
